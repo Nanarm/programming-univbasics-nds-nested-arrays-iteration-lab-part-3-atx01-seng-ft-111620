03892a7b6584ea1aec7315d1bdbaba333f7228ed
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-20 01:29:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ counter = 0
  
     inner_counter = 0
     while inner_counter < src[counter].count do
-      if src[counter].select(&:string?)
+      if src[counter].select { |n| n.String? }
       # if element.is_a?(String) 
       results_array << src[counter]
       inner_counter += 1
